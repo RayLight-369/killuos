0fcc727555ca6a9e4490b681efb829493288c656
Ray <buttrafay980@gmail.com>
2024-08-18 12:42:48 +0500

    update

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -74,26 +74,26 @@ const Contact = () => {
           <p className='text-center md:text-left text-[.90rem] md:text-[.95rem] text-[#F5F5F5] md:px-[4px]'>Boost your online presence—contact our web team to reach your goals.</p>
         </div>
 
-        <img className='md:absolute w-[45vw] md:top-[calc(50%-3rem)] md:-translate-y-1/2 object-cover md:right-12' src="/Imgs/contactillustration.png" alt="illustration" />
-        <div className='md:absolute w-[45vw] h-full aspect-square bg-[radial-gradient(50%_50%_at_50%_50%,rgba(31,29,27,0.15)_0%,#1F1D1B_100%)]  md:top-[calc(50%-3rem)] md:-translate-y-1/2 md:right-12'></div>
+        <img className='md:absolute md:w-[45vw] md:top-[calc(50%-3rem)] md:-translate-y-1/2 object-cover md:right-12' src="/Imgs/contactillustration.png" alt="illustration" />
+        <div className='absolute top-[80px] md:w-[45vw] h-[60vw] w-full aspect-square bg-[radial-gradient(50%_50%_at_50%_50%,rgba(31,29,27,0.15)_0%,#1F1D1B_100%)]  md:top-[calc(50%-3rem)] md:-translate-y-1/2 md:right-12'></div>
 
       </div>
 
-      <div id="contact-form" className='relative bg-[var(--bg-light)] flex justify-center items-center'>
-        <div className='relative -top-40 left-20  flex items-center'>
-          <div className='absolute -left-40 w-[24vw] h-[calc(65vh-80px)] bg-[var(--hero-bg)] shadow-xl'></div>
-          <div className='md:w-[65vw] md:h-[65vh] bg-white shadow-2xl flex pl-[calc(24vw-10rem+30px)] pr-32 py-[40px]'>
-            <div className='text-[var(--hero-bg)] w-full flex flex-col gap-8 relative'>
-              <h1 className='text-2xl font-bold'>Contact us</h1>
+      <div id="contact-form" className='relative bg-[var(--bg-light)] flex justify-center items-center md:top-0'>
+        <div className='relative md:-top-40 md:left-20 flex flex-col md:items-center md:flex-row'>
+          <div className='md:absolute md:-left-40 md:w-[24vw] md:h-[calc(65vh-80px)] bg-[var(--hero-bg)] shadow-xl'></div>
+          <div className='md:w-[65vw] md:h-[65vh] bg-white shadow-2xl flex flex-col md:flex-row md:pl-[calc(24vw-10rem+30px)] md:pr-32 md:py-[40px]'>
+            <div className='text-[var(--hero-bg)] w-screen md:w-full flex flex-col gap-8 relative p-10'>
+              <h1 className='text-2xl font-bold text-center md:text-left'>Contact us</h1>
               <div className='relative w-full h-full flex flex-col gap-5'>
-                <div className='w-full flex justify-between gap-10 h-fit'>
+                <div className='w-full flex flex-col md:flex-row justify-between gap-10 h-fit'>
                   <input type="text" placeholder='Enter your Name' value={ sender_name } onChange={ e => setName( e.target.value ) } className={ inputClassName + " flex-1" } />
                   <input type="text" placeholder='Enter your Email' value={ sender_email } onChange={ e => setEmail( e.target.value ) } className={ inputClassName + " flex-1" } />
                 </div>
                 <textarea type="text" placeholder='Enter Message...' value={ message } onChange={ e => setMsg( e.target.value ) } className={ inputClassName + " max-h-[calc(100%-160px)_!important] min-h-[30px]" } />
 
               </div>
-              <button disabled={ sending } onClick={ handleSubmit } className='absolute bottom-0 rounded w-24 px-3 py-[6px] border-none outline-none bg-[var(--button-bg)] text-white transition-all hover:scale-[1.03] hover:shadow-xl'>Send</button>
+              <button disabled={ sending } onClick={ handleSubmit } className='md:absolute md:bottom-0 rounded md:w-24 px-3 py-[6px] border-none outline-none bg-[var(--button-bg)] text-white transition-all hover:scale-[1.03] hover:shadow-xl'>Send</button>
             </div>
           </div>
         </div>
