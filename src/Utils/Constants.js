e75df7ba2d5d4253ccbf24740298a84a8f3e0c61
Ray <buttrafay980@gmail.com>
2024-09-11 16:25:01 +0500

    update

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -5,11 +5,11 @@ export const NavLinks = [
     name: "Home",
     path: "/#"
   }, {
-    name: "Contact",
-    path: "/#contact"
-  }, {
     name: "Services",
     path: "/#services"
+  }, {
+    name: "Contact",
+    path: "/#contact"
   }
 ];
 
